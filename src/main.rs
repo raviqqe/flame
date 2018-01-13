b1859d26f427d478e108d3a02474e87ab5a49d36
Yota Toyama <raviqqe@gmail.com>
2018-01-14 05:29:12 +0900

    Import futures related crates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-#![feature(integer_atomics)]
+#![feature(conservative_impl_trait, generators, integer_atomics, proc_macro)]
+
+extern crate futures_await as futures;
+extern crate futures_black_hole;
 
 mod arguments;
 mod thunk;
