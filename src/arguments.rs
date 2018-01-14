02cc13edadb5460ebf625b505d7261dfcdc308c5
Yota Toyama <raviqqe@gmail.com>
2018-01-14 16:04:28 +0900

    Implement arguments

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,4 +1,32 @@
 use value::*;
 
 #[derive(Clone, Debug)]
-pub struct Arguments(Value);
+pub struct Arguments {
+    positionals: Vec<Value>,
+    expanded_list: Value,
+    keywords: Vec<KeywordArgument>,
+    expanded_dicts: Vec<Value>,
+}
+
+impl Arguments {
+    pub fn new(ps: Vec<Value>, ks: Vec<KeywordArgument>, ds: Vec<Value>) -> Arguments {
+        Arguments {
+            positionals: ps,
+            expanded_list: Value::Invalid,
+            keywords: ks,
+            expanded_dicts: ds,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct KeywordArgument {
+    name: String,
+    value: Value,
+}
+
+impl KeywordArgument {
+    pub fn new(s: String, v: Value) -> Self {
+        KeywordArgument { name: s, value: v }
+    }
+}
--- src/value.rs
+++ src/value.rs
@@ -3,6 +3,7 @@ use normal::Normal;
 
 #[derive(Clone, Debug)]
 pub enum Value {
+    Invalid,
     Normal(Normal),
     Thunk(thunk::Thunk),
 }
