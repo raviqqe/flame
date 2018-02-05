83abce211b6bea053220cb39281f38abde47acaa
Yota Toyama <raviqqe@gmail.com>
2018-02-05 09:28:48 +0900

    TEMPORARY COMMIT

--- src/compile/compile.rs
+++ src/compile/compile.rs
@@ -1,7 +1,8 @@
 use super::super::ast::Statement;
 
 use super::effect::Effect;
+use super::error::CompileError;
 
-pub fn compile(ss: Vec<Statement>) -> Vec<Effect> {
-    vec![]
+pub fn compile(ss: Vec<Statement>) -> Result<Vec<Effect>, CompileError> {
+    Ok(vec![])
 }
