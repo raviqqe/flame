36b2735d07c2981146f4c90d2cb7d74019eacb09
Yota Toyama <raviqqe@gmail.com>
2018-03-11 09:34:18 +0900

    Add add and divide functions

--- src/core/number.rs
+++ src/core/number.rs
@@ -5,6 +5,26 @@ use super::signature::Signature;
 use super::value::Value;
 
 pure_function!(
+    ADD,
+    Signature::new(vec![], vec![], "ns".into(), vec![], vec![], "".into()),
+    add
+);
+
+#[async_move(boxed_send)]
+fn add(vs: Vec<Value>) -> Result<Value> {
+    let mut l = await!(vs[0].clone().list())?;
+    let mut n = 0.0;
+
+    while !l.is_empty() {
+        let m = await!(l.first()?.number())?;
+        n += m;
+        l = await!(l.rest())?;
+    }
+
+    Ok(n.into())
+}
+
+pure_function!(
     SUBTRACT,
     Signature::new(vec![], vec![], "ns".into(), vec![], vec![], "".into()),
     subtract
@@ -45,6 +65,27 @@ fn multiply(vs: Vec<Value>) -> Result<Value> {
     Ok(n.into())
 }
 
+pure_function!(
+    DIVIDE,
+    Signature::new(vec![], vec![], "ns".into(), vec![], vec![], "".into()),
+    divide
+);
+
+#[async_move(boxed_send)]
+fn divide(vs: Vec<Value>) -> Result<Value> {
+    let mut l = await!(vs[0].clone().list())?;
+    let mut n = await!(l.first()?.number())?;
+    l = await!(l.rest())?;
+
+    while !l.is_empty() {
+        let m = await!(l.first()?.number())?;
+        n /= m;
+        l = await!(l.rest())?;
+    }
+
+    Ok(n.into())
+}
+
 #[cfg(test)]
 mod test {
     use futures::executor::block_on;
@@ -52,6 +93,18 @@ mod test {
     use super::*;
 
     use super::super::utils::papp;
+
+    #[test]
+    fn add() {
+        for (xs, y) in vec![
+            (&[], 0.0),
+            (&[42.into()], 42.0),
+            (&[1.into(), 2.into(), 3.into()], 6.0),
+        ]: Vec<(&[Value], f64)>
+        {
+            assert_eq!(block_on(papp(ADD.clone(), xs).number()).unwrap(), y);
+        }
+    }
 
     #[test]
     fn subtract() {
@@ -80,5 +133,23 @@ mod test {
         {
             assert_eq!(block_on(papp(MULTIPLY.clone(), xs).number()).unwrap(), y);
         }
+    }
+
+    #[test]
+    fn divide() {
+        for (xs, y) in vec![
+            (&[0.into()], 0.0),
+            (&[42.into(), 1.into()], 42.0),
+            (&[1.into(), 2.into()], 0.5),
+            (&[1.into(), 2.into(), 2.into()], 0.25),
+        ]: Vec<(&[Value], f64)>
+        {
+            assert_eq!(block_on(papp(DIVIDE.clone(), xs).number()).unwrap(), y);
+        }
+    }
+
+    #[test]
+    fn divide_error() {
+        assert!(block_on(papp(DIVIDE.clone(), &[]).number()).is_err());
     }
 }
