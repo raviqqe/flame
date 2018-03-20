4d8a1f8a509067aec489b30d2d952be5cc2a12c7
Yota Toyama <raviqqe@gmail.com>
2018-03-20 15:30:36 +0900

    Make arguemnts fields public

--- src/ast/arguments.rs
+++ src/ast/arguments.rs
@@ -4,8 +4,8 @@ use super::keyword_argument::KeywordArgument;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Arguments {
-    positionals: Vec<Expansion<Expression>>,
-    keywords: Vec<Expansion<KeywordArgument>>,
+    pub positionals: Vec<Expansion<Expression>>,
+    pub keywords: Vec<Expansion<KeywordArgument>>,
 }
 
 impl Arguments {
