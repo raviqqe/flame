b73227c832cc883da8290a765438acee393ab64e
Yota Toyama <raviqqe@gmail.com>
2018-03-20 09:53:48 +0900

    Use Self

--- src/core/error.rs
+++ src/core/error.rs
@@ -33,11 +33,11 @@ impl Error {
         &self.0.message
     }
 
-    pub fn argument(m: &str) -> Error {
+    pub fn argument(m: &str) -> Self {
         Self::new("ArgumentError", m)
     }
 
-    pub fn runtime(m: &str) -> Error {
+    pub fn runtime(m: &str) -> Self {
         Self::new("RuntimeError", m)
     }
 
@@ -49,16 +49,16 @@ impl Error {
         Self::new("ImpureError", "impure value detected in pure context")
     }
 
-    pub fn value(m: &str) -> Error {
+    pub fn value(m: &str) -> Self {
         Self::new("ValueError", m)
     }
 
-    pub fn empty_list() -> Error {
+    pub fn empty_list() -> Self {
         Self::value("list is empty")
     }
 
     #[async_move]
-    pub fn key_not_found(v: Value) -> Result<Error> {
+    pub fn key_not_found(v: Value) -> Result<Self> {
         let s = await!(v.to_string())?;
 
         Ok(Self::new(
@@ -72,13 +72,13 @@ impl Error {
     }
 
     #[async_move]
-    pub fn typ(n: Normal, t: String) -> Result<Error> {
+    pub fn typ(n: Normal, t: String) -> Result<Self> {
         let s = await!(n.to_string())?;
         Ok(Self::typ_raw(&format!("{} is not a {}", s, t)))
     }
 
     #[async_move]
-    pub fn not_boolean(n: Normal) -> Result<Error> {
+    pub fn not_boolean(n: Normal) -> Result<Self> {
         await!(Self::typ(n, "boolean".to_string()))
     }
 
@@ -98,43 +98,43 @@ impl Error {
     }
 
     #[async_move]
-    pub fn not_collection(n: Normal) -> Result<Error> {
+    pub fn not_collection(n: Normal) -> Result<Self> {
         await!(Self::typ(n, "collection".to_string()))
     }
 
     #[async_move]
-    pub fn not_dictionary(n: Normal) -> Result<Error> {
+    pub fn not_dictionary(n: Normal) -> Result<Self> {
         await!(Self::typ(n, "dictionary".to_string()))
     }
 
     #[async_move]
-    pub fn not_function(n: Normal) -> Result<Error> {
+    pub fn not_function(n: Normal) -> Result<Self> {
         await!(Self::typ(n, "function".to_string()))
     }
 
     #[async_move]
-    pub fn not_list(n: Normal) -> Result<Error> {
+    pub fn not_list(n: Normal) -> Result<Self> {
         await!(Self::typ(n, "list".to_string()))
     }
 
     #[async_move]
-    pub fn not_nil(n: Normal) -> Result<Error> {
+    pub fn not_nil(n: Normal) -> Result<Self> {
         await!(Self::typ(n, "nil".to_string()))
     }
 
     #[async_move]
-    pub fn not_number(n: Normal) -> Result<Error> {
+    pub fn not_number(n: Normal) -> Result<Self> {
         await!(Self::typ(n, "number".to_string()))
     }
 
     #[async_move]
-    pub fn not_string(n: Normal) -> Result<Error> {
+    pub fn not_string(n: Normal) -> Result<Self> {
         await!(Self::typ(n, "string".to_string()))
     }
 }
 
 impl<E: error::Error> From<E> for Error {
     fn from(e: E) -> Self {
-        Error::runtime(e.description())
+        Self::runtime(e.description())
     }
 }
